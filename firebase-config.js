85ec11f3593bc75954ebffce415028c5b0c719ac
Madhu M <142919026+gitmadhu29@users.noreply.github.com>
2026-04-29 11:27:28 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwEL1uUx0M-fm5G9CrfSvP3fACLz11hbA",
+    authDomain: "gympro-new.firebaseapp.com",
+    databaseURL: "https://gympro-new-default-rtdb.firebaseio.com", // ✅ ADD THIS
+    projectId: "gympro-new",
+    storageBucket: "gympro-new.firebasestorage.app",
+    messagingSenderId: "332680897173",
+    appId: "1:332680897173:web:90ec514aa103cb7c10b19c",
+    measurementId: "G-28334E9441"
+};
+
+firebase.initializeApp(firebaseConfig);
